8b655c84ec717dbcd78ef5ec2710a7f509789488
filiucov <filiucov@mail.ru>
2018-01-07 12:28:21 +0000

    Setup geneating spec files when model is generated by default.

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,16 @@ module DancyApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                        fixtures: true,
+                        view_specs: false,
+                        helper_specs: false,
+                        routing_specs: false,
+                        request_specs: false,
+                        controller_spec: true
+      g.fixture_repalcement :factory_bot, dir: 'spec/factories'
+    end
   end
 end
